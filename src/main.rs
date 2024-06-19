33639490b2048f573adf74505fcde7270a6f1328
Alfred Dobradi <alfreddobradi@gmail.com>
2024-06-19 12:06:03 +0100

    feat: Endpoint to send status

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+mod toot;
+
 use axum::{
-    routing::get,
+    routing::{get, post},
+    http::StatusCode,
+    Json,
     Router,
 };
+use axum::response::IntoResponse;
+use serde::{Serialize, Deserialize};
+use serde_json::json;
+use uuid::Uuid;
+use crate::toot::create_toot;
 
 #[tokio::main]
 async fn main() {
@@ -11,7 +20,8 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/api/review", post(new_review));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -21,4 +31,42 @@ async fn main() {
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateReview {
+    url: String,
+    review: String,
+    schedule: String,
+}
+
+async fn new_review(
+    Json(input): Json<CreateReview>
+) -> impl IntoResponse {
+    let review = Review{
+        id: Uuid::new_v4(),
+        url: input.url,
+        review: input.review,
+        schedule: input.schedule,
+        post_url: "".to_string(),
+    };
+
+    tracing::info!("new review: {:?}", review);
+
+    match create_toot(review).await {
+        Ok(res) => (StatusCode::CREATED, Json(res)),
+        Err(e) => {
+            eprintln!("error: {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(json!("Internal Server Error")))
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Clone)]
+struct Review {
+    id: Uuid,
+    url: String,
+    review: String,
+    schedule: String,
+    post_url: String,
 }
---
+++ src/toot.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+use std::env;
+use anyhow::Result;
+use axum::http::{HeaderMap, HeaderValue};
+use serde_json::{json, Value};
+
+pub async fn create_toot(review: crate::Review) -> Result<Value> {
+    let debug_mode = env::var("OXP_DEBUG")?;
+    let mastodon_host = env::var("OXP_MASTODON_HOST")?;
+    let mut access_token = env::var("OXP_ACCESS_TOKEN")?;
+    access_token.insert_str(0, "Bearer ");
+
+    // tracing::info!("access_token_header: {}", access_token_header);
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static("application/json"));
+    headers.insert("Authorization", HeaderValue::from_str(access_token.as_str())?);
+
+    let r = format!("{}\nSpotify: {}", review.review, review.url);
+
+    let mut payload: HashMap<&str, &str> = HashMap::new();
+    payload.insert("status", r.as_str());
+
+    if debug_mode == "1" {
+        payload.insert("visibility", "private");
+    }
+
+    if review.schedule != "" {
+        payload.insert("scheduled_at", review.schedule.as_str());
+    }
+
+    let client = reqwest::Client::new();
+    let api_endpoint = format!("{}/api/v1/statuses", mastodon_host);
+    let res = client.post(api_endpoint)
+        .headers(headers)
+        .json(&payload)
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+
+    Ok(res)
+}
